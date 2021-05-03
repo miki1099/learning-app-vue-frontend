170128aae24bcf10f4c4e57d26b6308b42417784
Michal G <miki.glodek@wp.pl>
2021-05-03 19:29:48 +0200

    change learn home view

--- src/pages/Credits.vue
+++ src/pages/Credits.vue
@@ -38,4 +38,5 @@
   </div>
   <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
   <div>Icons made by <a href="https://www.flaticon.com/authors/mynamepong" title="mynamepong">mynamepong</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+  <div>Icons made by <a href="https://www.flaticon.com/authors/becris" title="Becris">Becris</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
 </template>
